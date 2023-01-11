519dd18b3f31c3ad55dc97e7da2d60b0d4d4f24f
PixulaKawai <benjacaniu47@gmail.com>
2023-01-11 05:39:54 -0600

    hice lo que pude

--- frontend/Data/Avisos.js
+++ frontend/Data/Avisos.js
@@ -1,34 +1,47 @@
 import axios from "axios";
 
-const getavisos = async()=>{
-    const response = await axios.get(`${process.env.SERVIDOR}/Msmboard/getAviso`)
+const getAvisos = async()=> {try{
+    const response = await axios.get(`${process.env.SERVIDOR}/MsmBoard/getAviso`)
     return response
-}
+} catch (error) {
+    console.log(error)
+    return error
+}}
 
-const sendEmail = async(email) =>{
-    const response = await axios.post(`${process.env.SERVIDOR}/Msmboard/post`,email)
+const sendEmail = async(email) =>{try{
+    const response = await axios.post(`${process.env.SERVIDOR}/MsmBoard/post`,email)
     return response
-}
+} catch (error) {
+    console.log(error)
+    return error
+}}
 
-const postAviso = async(avisos) =>{
-    const response = await axios.post(`${process.env.SERVIDOR}/Msmboard/postAviso`,avisos)
+const postAviso = async(avisos) =>{try{
+    const response = await axios.post(`${process.env.SERVIDOR}/MsmBoard/postAviso`,avisos)
     return response
-}
+} catch (error) {
+    console.log(error)
+    return error
+}}
 
-const deleteAviso = async(id) =>{
-    const response = await axios.delete(`${process.env.SERVIDOR}/Msmboard/delete/${id}`)
+const deleteAviso = async(id) =>{try{
+    const response = await axios.delete(`${process.env.SERVIDOR}/MsmBoard/delete/${id}`)
     return response
-}
+} catch (error) {
+    console.log(error)
+    return error
+}}
 
-const updateAviso = async(id, avisos) =>{
-    const response = await axios.put(`${process.env.SERVIDOR}/Msmboard/update/${id}`,avisos)
+const updateAviso = async(id, avisos) =>{try {
+    const response = await axios.put(`${process.env.SERVIDOR}/MsmBoard/update/${id}`,avisos)
     return response
-}
-
-
+}   catch (error) {
+    console.log(error)
+    return error
+}}
 
 module.exports ={
-    getavisos,
+    getAvisos,
     sendEmail,
     postAviso,
     deleteAviso,
--- frontend/pages/Avisos/CrearAviso.jsx
+++ frontend/pages/Avisos/CrearAviso.jsx
@@ -1,38 +1,44 @@
-import { useState, useEffect } from 'react';
-import {Select,Container,Heading,HStack,Input,Stack,Table,Th,Tr,Td, Thead, Tbody, Button, FormControl, FormLabel } from '@chakra-ui/react';
-import router from 'next/router';
-import Avisos from '../../Data/Avisos';
-import axios from 'axios';
-import {getAvisos} from '../../Data/Avisos';
-
+import {useState} from 'react'
+import {Container,Heading,Input, Button, FormControl, FormLabel } from '@chakra-ui/react'
+import {postAviso} from '../../Data/Avisos'
+import router from 'next/router'
+import Inputform from '../../components/Inputform'
 
 const CrearAviso = () => {
-    const [open, setOpen] = useState(false);
-    const [mensaje, setMensaje] = useState('');
-    const [fecha, setFecha] = useState(Date.now());
-    const [titulo, setTitulo] = useState([
-        {
-            titulo: '',
-            mensaje: '',
-        }
-    ]);
 
-    const handleSubmit = (e) => {
-        setTitulo({
-            ...titulo,
-            [e.target.name]: e.target.value
-        })
-        console.log("titulo = ", titulo.titulo);
-        console.log("mensaje = ", titulo.mensaje);   
-    }
-    const onSubmit = async(e) => {
+    const [mensaje, setmensaje] = useState([{
+        titulo:'',
+        mensaje: '',
+        dia: ''
+    }])
+
+    const handleChange = (e) => {
+    setmensaje({
+        ...mensaje,
+        [e.target.name]: e.target.value
+    })
+	}
+
+	const onSubmit = (e) => {
         e.preventDefault()
-        await Avisos(titulo.titulo, titulo.mensaje)
+        postAviso(mensaje).then (res => {
+            console.log(res);
+        })
     }
 
-    return (
-        <>
-        <header style={{backgroundColor: '#00bcd4', height: '95px'}}>
+const [open, setOpen] = useState(false);    
+return (
+    <>
+    <style>
+        {`
+            body {
+                background: #008080;
+            }
+        `}
+    </style>
+
+
+    <header style={{backgroundColor: '#00bcd4', height: '95px'}}>
                 <div style={{ display: "flex", justifyContent: "center", alignItems: "center" }}>
                     <Button variant="contained" color="primary" style={{backgroundColor: '#4caf50', padding: '8px 13px', borderRadius: '5px', color: '#fff'}} onClick={()=> router.push('../Avisos/CrearAviso')}>
                     Anuncios
@@ -51,50 +57,25 @@ const CrearAviso = () => {
                         <Button variant="contained" color="primary" style={{backgroundColor: 'black', padding: '8px 13px', borderRadius: '5px', color: '#fff'}} onClick={()=> router.push('../horarios/Cancel')}>
                         Cancela Horario
                         </Button>
-                        <Button variant="contained" color="primary" style={{backgroundColor: 'black', padding: '8px 13px', borderRadius: '5px', color: '#fff'}} onClick={()=> router.push('../horarios/Create')}>
-                        Crear Horario
-                        </Button>
-                        <Button variant="contained" color="primary" style={{backgroundColor: 'black', padding: '8px 13px', borderRadius: '5px', color: '#fff'}} onClick={()=> router.push('../horarios/Update')}>
-                        Actualizar Horario
-                        </Button>
-                        <Button variant="contained" color="primary" style={{backgroundColor: 'black', padding: '8px 13px', borderRadius: '5px', color: '#fff'}} onClick={()=> router.push('../horarios/Delete')}>
-                        Barrera Horario
-                        </Button>
                     </div>
                     }
                 </div>
             </header>
-            <style>
-                {`
-                    body {
-                        background: #008080;
-                    }
-                `}
-            </style>
-            <form onSubmit={handleSubmit}>
-            <input
-                type="text"
-                placeholder="Titulo"
-                value={titulo}
-                onChange={e => setTitulo(e.target.value)}
-            />
-            <input
-                type="text"
-                placeholder="Mensaje"
-                value={mensaje}
-                onChange={e => setMensaje(e.target.value)}
-            />
-            <input
-                type="date"
-                placeholder="Fecha"
-                value={fecha}
-                onChange={e => setFecha(e.target.value)}
-            />
-            <button type="submit">Enviar</button>
-        </form>
-        </>
 
-    )
-}
+    <Container maxW="2xl">
+    <Heading as="h2" size="md" textAlign="center" mt="10">Crear aviso</Heading>
+    <FormControl>
+        <FormLabel>Titulo</FormLabel>
+            <Input type= "text" isRequired="true" onChange={handleChange}/>
+        <FormLabel>Mensaje</FormLabel>
+            <Input value={mensaje.mensaje} size='lg' type= "text" isRequired="true" onChange={handleChange}/>
+        <FormLabel>Dia</FormLabel>    
+            <Input value={mensaje.dia} placeholder="Dia" name="dia" type= "date" isRequired="true"  onChange={handleChange}/>
+        <Button mt={4}  type="submit" onClick = {onSubmit}>Postear</Button>
+    </FormControl>
+    <Button colorScheme="green" variant="link" size="md"  onClick={()=> router.push('../../')}>Volver</Button>
+    </Container>
+    </>
+)}
 
-export default CrearAviso;
+export default CrearAviso   
